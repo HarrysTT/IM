a98aa5b13a61d02d9afe745463ac40a9089614ca
HarrysTT <297893894@qq.com>
2017-03-03 10:36:05 +0800

    升级Gradle从2.14.1到3.3；删除apt注解，改为annotationProcessor

--- app/src/main/java/com/xiey/im/im/module_main/MainActivity.java
+++ app/src/main/java/com/xiey/im/im/module_main/MainActivity.java
@@ -7,13 +7,19 @@ import android.widget.TextView;
 import com.xiey.im.im.BuildConfig;
 import com.xiey.im.im.R;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.tv_test)
+    TextView tv_test;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        TextView tv_test = (TextView) findViewById(R.id.tv_test);
+        ButterKnife.bind(this);
         if (BuildConfig.isTestEnvironment) {
             tv_test.setText("true");
         } else {
